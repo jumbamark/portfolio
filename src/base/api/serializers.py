f658101a8d85d84515f66ce7e89aa49439dce3b6
Mark Jumba <jumbamark@yahoo.com>
2022-03-04 16:25:39 +0300

    Changed the project's working tree

--- src/base/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from base.models import Question
-from rest_framework import serializers
-
-
-class QuestionSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Question
-        fields = "__all__"
